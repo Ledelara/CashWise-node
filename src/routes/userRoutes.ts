df37093ca8fe007a5b295a49e3754ecb6607ee2e
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-01-26 17:35:00 -0300

    ♻️ refactor: refatorando types

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { IUser } from '../types/userTypes';
+import { IUser } from '../types/@Types';
 import { deleteUser, updateUser } from '../services/authService';
 
 const router = express.Router();
--- src/types/userTypes.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser {
-    name: string;
-    email: string;
-    password: string;
-    transactionPassword: string;
-    balance?: number;
-    accountNumber?: string;
-}
