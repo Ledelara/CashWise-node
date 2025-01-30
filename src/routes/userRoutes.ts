b9221f64afe3922269691cd3a49120fef3d67d6f
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2025-01-30 17:36:39 -0300

    ♻️ refactor: ESLint

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,20 @@
-import express from 'express';
-import { depositAmount, getStatement, getUserProfile, handleTransaction, transferAmount, withdrawAmount } from '../controllers/userController';
+import express from "express";
+import {
+  depositAmount,
+  getStatement,
+  getUserProfile,
+  handleTransaction,
+  transferAmount,
+  withdrawAmount,
+} from "../controllers/userController";
 
 const router = express.Router();
 
-router.get('/user/statement/:userId', getStatement);
-router.get('/user/profile/:userId', getUserProfile);
-router.post('/user/:id/deposit', depositAmount);
-router.post('/user/transfer', transferAmount);
-router.post('/user/transaction', handleTransaction);
-router.put('/user/withdraw/:id', withdrawAmount);
+router.get("/user/statement/:userId", getStatement);
+router.get("/user/profile/:userId", getUserProfile);
+router.post("/user/:id/deposit", depositAmount);
+router.post("/user/transfer", transferAmount);
+router.post("/user/transaction", handleTransaction);
+router.put("/user/withdraw/:id", withdrawAmount);
 
-export default router; 
+export default router;
